e91cf191e605cd9d1b806aeacda7e70e1de942a2
A Jerram <149675542+AndrewJerram@users.noreply.github.com>
2026-03-15 16:55:02 +0000

    finished frontend config

---
+++ frontend/src/api/client.ts
@@ -0,0 +1,9 @@
+export const apiClient = async (url: string) => {
+  const response = await fetch(url)
+
+  if (!response.ok) {
+    throw new Error("API request failed")
+  }
+
+  return response.json()
+}
