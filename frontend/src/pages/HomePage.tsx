b953635b0ea06e5e6613eb14b41fb2d234740c9e
A Jerram <149675542+AndrewJerram@users.noreply.github.com>
2026-03-15 16:57:43 +0000

    fix

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ const HomePage = () => {
   return (
     <div>
       <h1>Welcome</h1>
-      <p>Frontend is running successfully 🎉</p>
+      <p>Frontend is running successfully </p>
     </div>
   )
 }
