3779c4427c2da26833c36f11e048895a55bd86b8
adrianwieclaw <musical.clasher@gmail.com>
2026-04-29 00:39:02 +0100

    #7 fixed user and role entities and repositories, plus migrations

--- backend/src/main/java/com/smartcity/reporting/user/repository/RoleRepository.java
+++ backend/src/main/java/com/smartcity/reporting/user/repository/RoleRepository.java
@@ -1,3 +1,11 @@
+package com.smartcity.reporting.user.repository;
+
+import com.smartcity.reporting.user.entity.Role;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.Optional;
+
 @ApplicationScoped
 public class RoleRepository implements PanacheRepository<Role> {
 
